48b867593774a8589dc9c24dc9622725728abcb5
Giovanni Moratto <morattotrabalho@gmail.com>
2021-08-07 01:26:56 -0300

    v2.0

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -7,10 +7,18 @@ import br.com.alura.bytebank.modelo.Endereco
  */
 
 fun main() {
-    val endereco = Endereco(logradouro = "Rua vergueiro", complemento = "Alura")
-    val enderecoNovo = Endereco(bairro = "Vila Mariana", numero = 1000)
+    val endereco = Endereco(
+        logradouro = "Rua vergueiro",
+        complemento = "Alura",
+        cep = "00000-0700"
+    )
+    val enderecoNovo = Endereco(
+        logradouro = "Rua vergueiro",
+        complemento = "Alura",
+        cep = "00000-0700"
+    )
 
-    println(endereco.equals(endereco))
+    println(endereco.equals(enderecoNovo))
 
     println(endereco.hashCode())
     println(enderecoNovo.hashCode())
