8e38817729e0af153fd71fd83ee5f74eccb665d1
Giovanni Moratto <morattotrabalho@gmail.com>
2021-08-07 01:09:42 -0300

    v1.5

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,5 +1,9 @@
 package br.com.alura.bytebank
 
+import br.com.alura.bytebank.modelo.Cliente
+import br.com.alura.bytebank.modelo.ContaCorrente
+import br.com.alura.bytebank.modelo.ContaPoupanca
+import br.com.alura.bytebank.modelo.totalContas
 import br.com.alura.bytebank.teste.testaContasDiferentes
 
 /**
@@ -7,5 +11,11 @@ import br.com.alura.bytebank.teste.testaContasDiferentes
  */
 
 fun main() {
+    val giovanni = Cliente(nome = "Giovanni", cpf = "", senha = 1)
+    val contaPoupanca = ContaPoupanca(titular = giovanni, numero = 1000)
+    val contaCorrente = ContaCorrente(titular = giovanni, numero = 1001)
+
     testaContasDiferentes()
+
+    println("Total de contas: $totalContas")
 }
--- src/main/kotlin/br/com/alura/bytebank/modelo/Conta.kt
+++ src/main/kotlin/br/com/alura/bytebank/modelo/Conta.kt
@@ -4,12 +4,20 @@ package br.com.alura.bytebank.modelo
  *@Author giovanni.moratto
  */
 
+var totalContas = 0
+    private set
+
 abstract class Conta(
     var titular: Cliente,
     val numero: Int
 ) {
     var saldo = 0.0
         protected set
+
+    init {
+        println("Criando conta")
+        totalContas++
+    }
 
     fun deposita(valor: Double) {
         if (valor > 0) {
@@ -27,4 +35,45 @@ abstract class Conta(
         }
         return false
     }
+
+}
+
+class ContaCorrente(
+    titular: Cliente,
+    numero: Int
+) : Conta(
+    titular = titular,
+    numero = numero
+) {
+
+    init {
+        totalContas++
+    }
+
+    override fun saca(valor: Double) {
+        val valorComTaxa = valor + 0.1
+        if (this.saldo >= valorComTaxa) {
+            this.saldo -= valorComTaxa
+        }
+    }
+}
+
+class ContaPoupanca(
+    titular: Cliente,
+    numero: Int
+) : Conta(
+    titular = titular,
+    numero = numero
+) {
+
+    init {
+        totalContas++
+    }
+
+    override fun saca(valor: Double) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor
+        }
+    }
+
 }
