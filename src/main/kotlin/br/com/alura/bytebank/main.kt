d8679e31517c28f222cc8f8c708e69326982f7cd
Giovanni Moratto <morattotrabalho@gmail.com>
2021-08-07 01:19:17 -0300

    v1.8

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,30 +1,22 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.modelo.*
-import br.com.alura.bytebank.teste.testaContasDiferentes
+import br.com.alura.bytebank.modelo.Endereco
 
 /**
  *@Author giovanni.moratto
  */
 
 fun main() {
+    val endereco = Endereco()
+    val objeto: Any = Any()
+    imprime(objeto)
+    imprime(1)
+    imprime(1.0)
+    val teste: Any = imprime(endereco)
+    println(teste)
+}
 
-    val fran = object : Autenticavel {
-        val nome: String = "Fran"
-        val cpf: String = "111.111.111-11"
-        val senha: Int = 1000
-
-        override fun autentica(senha: Int) = this.senha == senha
-    }
-    val sistemaInterno = SistemaInterno()
-    sistemaInterno.entra(fran, 1000)
-    println("nome do cliente ${fran.nome}")
-
-    val giovanni = Cliente(nome = "Giovanni", cpf = "", senha = 1)
-    val contaPoupanca = ContaPoupanca(titular = giovanni, numero = 1000)
-    val contaCorrente = ContaCorrente(titular = giovanni, numero = 1001)
-
-    testaContasDiferentes()
-
-    println("Total de contas: ${Conta.total}")
+fun imprime(valor: Any): Any {
+    println(valor)
+    return valor
 }
