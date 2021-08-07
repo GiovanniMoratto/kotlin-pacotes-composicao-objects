5aae45974514f42a40713ecbaf772e2e83276b79
Giovanni Moratto <morattotrabalho@gmail.com>
2021-08-07 01:25:19 -0300

    v1.9

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -7,13 +7,22 @@ import br.com.alura.bytebank.modelo.Endereco
  */
 
 fun main() {
-    val endereco = Endereco()
-    val objeto: Any = Any()
-    imprime(objeto)
-    imprime(1)
-    imprime(1.0)
-    val teste: Any = imprime(endereco)
-    println(teste)
+    val endereco = Endereco(logradouro = "Rua vergueiro", complemento = "Alura")
+    val enderecoNovo = Endereco(bairro = "Vila Mariana", numero = 1000)
+
+    println(endereco.equals(endereco))
+
+    println(endereco.hashCode())
+    println(enderecoNovo.hashCode())
+
+    println(endereco)
+    println(enderecoNovo)
+
+    println(
+        "${endereco.javaClass}@${
+            Integer.toHexString(endereco.hashCode())
+        }"
+    )
 }
 
 fun imprime(valor: Any): Any {
