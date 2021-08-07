ffc8d295371e5e310d31ac35cf8c13a73ca0b468
Giovanni Moratto <morattotrabalho@gmail.com>
2021-08-07 01:12:11 -0300

    v1.6

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,9 +1,6 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.modelo.Cliente
-import br.com.alura.bytebank.modelo.ContaCorrente
-import br.com.alura.bytebank.modelo.ContaPoupanca
-import br.com.alura.bytebank.modelo.totalContas
+import br.com.alura.bytebank.modelo.*
 import br.com.alura.bytebank.teste.testaContasDiferentes
 
 /**
@@ -11,6 +8,20 @@ import br.com.alura.bytebank.teste.testaContasDiferentes
  */
 
 fun main() {
+
+    val fran = object : Autenticavel {
+        val nome: String = "Fran"
+        val cpf: String = "111.111.111-11"
+        val senha: Int = 1000
+
+        override fun autentica(senha: Int) = this.senha == senha
+    }
+
+    val sistemaInterno = SistemaInterno()
+    sistemaInterno.entra(fran, 1000)
+
+    println("nome do cliente ${fran.nome}")
+
     val giovanni = Cliente(nome = "Giovanni", cpf = "", senha = 1)
     val contaPoupanca = ContaPoupanca(titular = giovanni, numero = 1000)
     val contaCorrente = ContaCorrente(titular = giovanni, numero = 1001)
